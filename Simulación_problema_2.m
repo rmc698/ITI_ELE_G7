70e99cc10a6368e337061f3cf0eb7ddffd751853
rmc698 <99616836+rmc698@users.noreply.github.com>
2025-10-20 19:19:13 +0200

    Simulation exercise name change

--- Simulación_problema_2.m
+++ Simulación_problema_2.m
@@ -1,31 +1,3 @@
-%% Problema 1
-R = 330
-L = 0.15
-Vs = 15
-tao = L/R
-
-
-time = 0:10*10^-7:tao*20;
-
-If = Vs/R
-
-I = If*(1-exp(-time/tao));
-
-I_tao = If*(1-exp(-1));
-
-close all
-figure(1)
-plot(time,I)
-grid on 
-xlabel('Time (s)')
-ylabel('Current (A)')
-hold on 
-plot(tao, I_tao, 'ro', 'MarkerSize', 5, 'MarkerFaceColor', 'r')
-asintota = If*ones(length(time));
-plot(time,asintota,'k')
-% plot(x_punto, y_punto, 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r')
-
-
 %% Simulación 
 clear all
 close all 
@@ -73,4 +45,5 @@ xlabel('Tiempo [s]')
 ylabel('Corriente [A]')
 % ylim([0 11])
 xlim([0 0.0005])
+
 xline(0.00013, 'r--', 'Pico de corriente', 'LineWidth', 2)
